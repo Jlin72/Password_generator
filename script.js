c2c0cfd50aa0ae17229aacf2cd35bb31c8db22e2
Jhonny Lin <jhonnylin72@gmail.com>
2020-10-15 16:18:26 -0400

    added comments to the javascript file

--- script.js
+++ script.js
@@ -36,29 +36,47 @@ function generatePassword() {
   if (!passwordNumber && !passwordChar && !passwordUpper  && !passwordLower) {alert("Please choose a criteria");}
   // These line of code is if the user selects the all the options available for the password
   else if (passwordNumber && passwordChar && passwordUpper && passwordLower) {choices = spChar.concat (number, upperCase, lowerCase);}
-  // These next lines of code are if the user selects only 4 different combinations for their password
+  
+  // These next lines of code are in case the user selects only 3 different combinations for their password
+    // This first combination is if the user wants their password to have a number, special character and upper cases.
   else if (passwordNumber && passwordChar && passwordUpper) {choices = number.concat (upperCase, spChar);}
+    // This line of code is if the user wants their password to contain numbers, lower cases and upper cases
   else if (passwordNumber && passwordLower && passwordUpper) {choices = number.concat (upperCase, lowerCase);}
+    // This line of code is if the user wants their password to have a number, special character and a lower case
   else if (passwordNumber && passwordChar && passwordLower) {choices = number.concat (spChar, lowerCase);}
+    // This line of code is if the user wants their password to have a lower case, special character and an upper case
   else if (passwordLower && passwordChar && passwordUpper) {choices = lowerCase.concat (upperCase, spChar);}
+  
   // These next lines of code are only if the user selects 2 different possible combinations for their password
+    // This next line of code are only if the user wants their password to have only numbers and special characters
   else if (passwordNumber && passwordChar) {choices = number.concat (spChar);}
+    // This next line of code are only if the user wants their password to have numbers and upper case letters
   else if (passwordNumber && passwordUpper) {choices = number.concat (upperCase);}
+    // This next line of code are only if the user wants their password to have numbers and lower case letters
   else if (passwordNumber && passwordLower) {choices = number.concat (lowerCase);}
+    // This next line of code are only if the user wants their password to have only special characters and upper case letters
   else if (passwordChar && passwordUpper) {choices = spChar.concat (upperCase);}
+    // This next line of code are only if the user wants their password to contain both special characters and lower case letters
   else if (passwordChar && passwordLower) {choices = spChar.concat (lowerCase);}
+    // This next line of code are only if the user wnats their password to contain both upper and lower case letters
   else if (passwordUpper && passwordLower) {choices = upperCase.concat (lowerCase);}
+  
   // These next lines of code are only if the user selects only one option for the password
+    // This line of code is in case the user only wants number on their password
   else if (passwordNumber) {choices = number;}
+    // This line of code is in case the user only wants special characters on their password
   else if (passwordChar) {choices = spChar;}
+    // This line of code is in case the user only wants upper case letters on their password
   else if (passwordUpper) {choices = upperCase;}
+    // This line of code is in case the user only wants lower case letters on their password
   else if (passwordLower) {choices = lowerCase;}
   // empty array that will contain the new empty password
   var randomPassword = [];
-  // This next line of code is the loop requiered for the generation of the password
+  // This next line of code is the loop requiered for the creation of the password
   for (var i = 0; i<passwordLength; i++) {
     var allChoices = choices [Math.floor(Math.random()* choices.length)]; randomPassword.push(allChoices);
   }
+  // This next line of code will be used so that the password will be displayed on the box
   return randomPassword.join("")
   }
 
